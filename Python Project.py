e4150b6fa7c83cac572108c224097cad7bd55f95
Ben Wratten <benjamin.wratten@btinternet.com>
2020-08-05 20:02:04 +0100

    pen size doubled

--- Python Project.py
+++ Python Project.py
@@ -9,7 +9,7 @@ egg.color("#F4A896", "#358597")
 
 n = int(input("Integer: \n"))
 egg.begin_fill()
-egg.pensize(5)
+egg.pensize(10)
 for i in range(n):
     egg.forward(50)
     egg.left(180-(180*(n-2)/n))
