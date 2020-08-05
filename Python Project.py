5d2bf2a3690cbc142812ef43f83382390914287c
Oscar Man <oscar.t.f.man@outlook.com>
2020-08-05 19:46:10 +0100

    shape drawing

--- Python Project.py
+++ Python Project.py
@@ -3,6 +3,14 @@
 import math
 import turtle
 
+egg = turtle.Turtle()
+
+n = int(input("Integer: \n"))
+for i in range(n):
+    egg.forward(50)
+    egg.left(180-(180*(n-2)/n))
+turtle.done()
+
 print("Hello World")
 print("Goodbye World")
 
