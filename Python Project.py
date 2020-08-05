8b7c61a1ac40f24c3116c09aa9959ed8c710ccf3
Oscar Man <oscar.t.f.man@outlook.com>
2020-08-05 19:52:37 +0100

    colour

--- Python Project.py
+++ Python Project.py
@@ -4,11 +4,15 @@ import math
 import turtle
 
 egg = turtle.Turtle()
+egg.color("#F4A896", "#358597")
 
 n = int(input("Integer: \n"))
+egg.begin_fill()
+egg.pensize(5)
 for i in range(n):
     egg.forward(50)
     egg.left(180-(180*(n-2)/n))
+egg.end_fill()
 turtle.done()
 
 print("Hello World")
