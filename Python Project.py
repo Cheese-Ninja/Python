2549fab89259de2de0ae11b7e55c70672e891545
Oscar Man <oscar.t.f.man@outlook.com>
2020-08-06 17:18:37 +0100

    cut turtle, added inputs

--- Python Project.py
+++ Python Project.py
@@ -4,67 +4,11 @@ import math
 import turtle
 import random
 
-egg = turtle.Turtle()
-egg.color("#F4A896", "#358597")
+# Asking User for inputs
 
-def semicircle(size):
-    for i in range(10):
-        egg.forward(size)
-        egg.left(18)
-
-# Main triangle
-
-
-egg.begin_fill()
-egg.pensize(10)
-for i in range(3):
-    egg.forward(200)
-    egg.left(120)
-egg.end_fill()
-egg.pensize(3)
-
-#C
-
-egg.color("#000000", "#000000")
-egg.penup()
-egg.forward(240)
-egg.right(180)
-egg.pendown()
-semicircle(5)
-egg.forward(5)
-
-#A
-
-egg.penup()
-egg.left(120)
-egg.forward(250)
-egg.pendown()
-for i in range(2):
-    egg.forward(30)
-    egg.left(120)
-egg.left(60)
-egg.forward(15)
-egg.right(60)
-egg.forward(15)
-
-#B
-
-egg.penup()
-egg.right(180)
-egg.forward(15)
-egg.left(60)
-egg.forward(260)
-egg.left(120)
-egg.pendown()
-for i in range(2):
-    semicircle(3)
-    egg.right(162)
-egg.left(235)
-egg.forward(40)
-
-egg.pensize(10)
-for i in range(n):
-    egg.forward(50)
-    egg.left(180-(180*(n-2)/n))
-egg.end_fill()
-turtle.done()
+AA = float(input("What is Angle A?\ If unknown, input 0.0\n"))
+AB = float(input("What is Angle B?\ If unknown, input 0.0\n"))
+AC = float(input("What is Angle C?\ If unknown, input 0.0\n"))
+sa = float(input("What is Side a?\ If unknown, input 0.0\n"))
+sb = float(input("What is Side b?\ If unknown, input 0.0\n"))
+sc = float(input("What is Side c?\ If unknown, input 0.0\n"))
