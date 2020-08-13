bcd9c96fe1c7115f8843f3dff68966dd69e9baa4
Oscar Man <oscar.t.f.man@outlook.com>
2020-08-13 19:43:57 +0100

    .

--- Python Project.py
+++ Python Project.py
@@ -6,9 +6,9 @@ import random
 
 # Asking User for inputs
 
-AA = float(input("What is Angle A?\ If unknown, input 0.0\n"))
-AB = float(input("What is Angle B?\ If unknown, input 0.0\n"))
-AC = float(input("What is Angle C?\ If unknown, input 0.0\n"))
-sa = float(input("What is Side a?\ If unknown, input 0.0\n"))
-sb = float(input("What is Side b?\ If unknown, input 0.0\n"))
-sc = float(input("What is Side c?\ If unknown, input 0.0\n"))
+AA = float(input("What is Angle A?\ If unknown, input 0\n"))
+AB = float(input("What is Angle B?\ If unknown, input 0\n"))
+AC = float(input("What is Angle C?\ If unknown, input 0\n"))
+sa = float(input("What is Side a?\ If unknown, input 0\n"))
+sb = float(input("What is Side b?\ If unknown, input 0\n"))
+sc = float(input("What is Side c?\ If unknown, input 0\n"))
