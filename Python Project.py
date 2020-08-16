5e996ea5b551a653e600fa826e709dec31e4deaf
Oscar Man <oscar.t.f.man@outlook.com>
2020-08-17 00:35:21 +0100

    bug fixes pythagoras finished i think

--- Python Project.py
+++ Python Project.py
@@ -6,12 +6,24 @@ import random
 
 # Asking User for inputs
 
-AA = float(input("What is Angle A?\ If unknown, input 0\n"))
-AB = float(input("What is Angle B?\ If unknown, input 0\n"))
-AC = float(input("What is Angle C?\ If unknown, input 0\n"))
-sa = float(input("What is Side a?\ If unknown, input 0\n"))
-sb = float(input("What is Side b?\ If unknown, input 0\n"))
-sc = float(input("What is Side c?\ If unknown, input 0\n"))
+AA = float(input("What is Angle A? If unknown, input 0\n"))
+AB = float(input("What is Angle B? If unknown, input 0\n"))
+AC = float(input("What is Angle C? If unknown, input 0\n"))
+sa = float(input("What is Side a? If unknown, input 0\n"))
+sb = float(input("What is Side b? If unknown, input 0\n"))
+sc = float(input("What is Side c? If unknown, input 0\n"))
+
+HypA = False
+HypB = False
+HypC = False
+
+if AA == 90:
+    HypA = True
+if AB == 90:
+    HypB = True
+if AC == 90:
+    HypC = True
+
 
 def twosides():
     if sa != 0 and sb != 0 and sc == 0:
@@ -23,13 +35,30 @@ def twosides():
     else:
         return 0
 
+# Pythagoras
+
 if twosides() > 0:
     if twosides() == 1:
-        sc = math.sqrt(math.pow(sa, 2) + math.pow(sb, 2))
-        print(sc)
+        if HypA == True:
+            sc = math.sqrt(math.pow(sa, 2) - math.pow(sb, 2))
+        elif HypB == True:
+            sc = math.sqrt(math.pow(sb, 2) - math.pow(sa, 2))
+        elif HypA == True:
+            sc = math.sqrt(math.pow(sa, 2) + math.pow(sb, 2))
+        print("Side c is " + str(sc))
     if twosides() == 2:
-        sb = math.sqrt(math.pow(sa, 2) + math.pow(sc, 2))
-        print(sc)
+        if HypA == True:
+            sb = math.sqrt(math.pow(sa, 2) - math.pow(sc, 2))
+        elif HypB == True:
+            sb = math.sqrt(math.pow(sa, 2) + math.pow(sc, 2))
+        elif HypC == True:
+            sb = math.sqrt(math.pow(sc, 2) - math.pow(sa, 2))
+        print("Side b is " + str(sb))
     if twosides() == 3:
-        sa = math.sqrt(math.pow(sb, 2) + math.pow(sc, 2))
-        print(sc)
+        if HypA == True:
+            sa = math.sqrt(math.pow(sb, 2) + math.pow(sc, 2))
+        elif HypB == True:
+            sa = math.sqrt(math.pow(sb, 2) - math.pow(sc, 2))
+        elif HypC == True:
+            sa = math.sqrt(math.pow(sc, 2) - math.pow(sb, 2))
+        print("Side a is " + str(sa))
